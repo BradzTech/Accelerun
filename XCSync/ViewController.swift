64cdfceb85ad8f76b0c972440bcee9a14d0a3199
BradzTech <brad@bradztechbuzz.com>
2017-06-09 10:49:43 -0400

    Initial Commit

---
+++ XCSync/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCSync
+//
+//  Created by Bradley Klemick on 6/9/17.
+//  Copyright © 2017 BradzTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
